1fcbe613cd63d4b1bc4ab4fff3bf66cccf9d66ad
Homer Glascock <hopglascock@Hops-MBP.lan>
2024-08-13 16:41:12 -0500

    docker compose

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import express from 'express';
+import fs from 'fs';
+
 
 const app = express();
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello hop!');
+});
+
+
 app.listen(4000, () => {
   console.log(`server running on port 4000`);
 });
